6677bccfcdd57a74d0b6d779cb14010ee613ad24
animevn <haanhth21@gmail.com>
2019-12-24 15:27:20 +0700

    ok

--- app/src/main/java/com/haanhgs/app/inlinenotificationtaskstack/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/inlinenotificationtaskstack/MainActivity.java
@@ -1,8 +1,17 @@
 package com.haanhgs.app.inlinenotificationtaskstack;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.RemoteInput;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -12,16 +21,91 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.bnSend)
     Button bnSend;
 
+    public static final String RESULT = "result_code";
+    public static final int NOTIFICATION_ID = 99999;
+
+    private static final String CHANNELID = "channel_id";
+    private static final int REQUEST = 999;
+    private NotificationManager manager;
+    private TaskStackBuilder builder;
+    private RemoteInput remoteInput;
+    private PendingIntent pendingIntent;
+    private NotificationCompat.Action action;
+
+    private void createChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(CHANNELID,
+                    "Notification", NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("Broadcast");
+            channel.enableLights(true);
+            channel.enableVibration(true);
+            channel.setLightColor(Color.RED);
+            manager.createNotificationChannel(channel);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        createChannel();
     }
 
+    private void createTaskStack(){
+        builder = TaskStackBuilder.create(this);
+        builder.addParentStack(MainActivity.class);
+    }
 
+    //if need to open MainActivity from notification, can remove this
+    private void createIntentToOpenMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        builder.addNextIntent(intent);
+    }
+
+    //open notification activity when click reply
+    private void createIntentToOpenNotificationActivity(){
+        Intent intent = new Intent(this, NotificationReply.class);
+        builder.addNextIntent(intent);
+    }
+
+    private void createRemoteInput(){
+        remoteInput = new RemoteInput.Builder(RESULT).setLabel("Reply").build();
+    }
+
+    private void createPendingIntent(){
+        pendingIntent = builder.getPendingIntent(REQUEST, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private void addRemoteInputToNotificationAction(){
+        action = new NotificationCompat.Action
+                .Builder(R.drawable.icon_foreground, " Reply", pendingIntent)
+                .addRemoteInput(remoteInput)
+                .build();
+    }
+
+    private void buildNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNELID)
+                .setSmallIcon(R.drawable.icon_foreground)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setContentTitle("Reply")
+                .setContentText("Hi")
+                .addAction(action);
+        manager.notify(NOTIFICATION_ID, builder.build());
+    }
 
     @OnClick(R.id.bnSend)
-    public void onViewClicked() {
+    public void onViewClicked(){
+        createTaskStack();
+        createIntentToOpenMainActivity();
+        createIntentToOpenNotificationActivity();
+        createRemoteInput();
+        createPendingIntent();
+        addRemoteInputToNotificationAction();
+        buildNotification();
     }
 }
--- app/src/main/java/com/haanhgs/app/inlinenotificationtaskstack/NotificationReply.java
+++ app/src/main/java/com/haanhgs/app/inlinenotificationtaskstack/NotificationReply.java
@@ -1,10 +1,14 @@
 package com.haanhgs.app.inlinenotificationtaskstack;
 
+import android.app.NotificationManager;
+import android.app.RemoteInput;
 import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import static com.haanhgs.app.inlinenotificationtaskstack.MainActivity.NOTIFICATION_ID;
+import static com.haanhgs.app.inlinenotificationtaskstack.MainActivity.RESULT;
 
 public class NotificationReply extends AppCompatActivity {
 
@@ -12,7 +16,11 @@ public class NotificationReply extends AppCompatActivity {
     TextView tvReceived;
 
     private void getCharFromInline(Bundle bundle){
-
+        CharSequence message = "";
+        if (bundle != null){
+            message = bundle.getCharSequence(RESULT);
+        }
+        tvReceived.setText(message);
     }
 
     @Override
@@ -21,6 +29,10 @@ public class NotificationReply extends AppCompatActivity {
         setContentView(R.layout.activity_notification_reply);
         ButterKnife.bind(this);
 
+        Bundle remote = RemoteInput.getResultsFromIntent(getIntent());
+        getCharFromInline(remote);
+        NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        if (manager != null) manager.cancel(NOTIFICATION_ID);
     }
 
 }
